da9779d1d37c0340d45dafbc8b461b3955d6b0f8
Andrey Skabelin <andrey.skabelin@gmail.com>
2015-07-30 11:07:11 +0300

    result

--- lib/result.py
+++ lib/result.py
@@ -7,7 +7,7 @@ import settings
 def create_ga_screen_result(task_uid):
     result_path = os.path.join(settings.RESULTS_FOLDER, task_uid)
     result = {}
-    for screen_type in 'organic', 'month_comparison', 'year_comparison':
+    for screen_type in 'organic', 'month_comparison', 'year_comparison', 'segment':
         with open(os.path.join(result_path, screen_type + '.png')) as f:
             content = f.read()
             result[screen_type] = base64.b64encode(content)
