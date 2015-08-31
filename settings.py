6bffd86e15f40ca746699968410ef3f3f9fb5676
Brytkov Dmitriy <d.brytkov@dev.kokoc.com>
2015-08-31 21:30:01 +0300

    set new proxy

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ CELERY_DISABLE_RATE_LIMITS = True
 # CELERY_ALWAYS_EAGER = True
 
 # Отключил работу через проксю, так как Google ее детектит как робота.
-# PROXY = '46.161.16.2:8888'
+PROXY = '31.184.200.129:8888'
 
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 RESULTS_FOLDER = os.path.join(PROJECT_PATH, 'results')
