f0fd80639e807af3a56cd5e911199968f9ddf25c
Brytkov Dmitriy <d.brytkov@dev.kokoc.com>
2015-08-31 21:36:23 +0300

    fix bug with segment file

--- lib/result.py
+++ lib/result.py
@@ -8,7 +8,10 @@ def create_ga_screen_result(task_uid):
     result_path = os.path.join(settings.RESULTS_FOLDER, task_uid)
     result = {}
     for screen_type in 'organic', 'month_comparison', 'year_comparison', 'segment':
-        with open(os.path.join(result_path, screen_type + '.png')) as f:
-            content = f.read()
-            result[screen_type] = base64.b64encode(content)
+        file_path = os.path.join(result_path, screen_type + '.png')
+        if os.path.exists(file_path):
+            with open(file_path) as f:
+                content = f.read()
+                result[screen_type] = base64.b64encode(content)
+
     return result
